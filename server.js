ab3ea7bc78fcaee0db83b672525dd915cdc94940
spaghettimedic <dsteiner247@gmail.com>
2021-12-13 06:47:32 -0500

    integrated all front-end code and updated functionality of it to serve page data

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+// 'middleware' to serve static files (font-end files: css and js)
+// this instructs the server to make everything in the 'public' directory into static resources
+// so that a server endpoint does not have to be specified for every single asset (front-end code such as css and js)
+app.use(express.static('public')); 
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -95,6 +99,22 @@ app.get('/api/animals/:id', (req, res) => {
   } else {
     res.send(404);
   }
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './pulic/index.html'));
 });
 
 app.post('/api/animals', (req, res) => {
