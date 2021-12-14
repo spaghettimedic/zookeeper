9f9d81261b02cd1ae624dbae2d5e8cc7d440ed6a
spaghettimedic <dsteiner247@gmail.com>
2021-12-13 21:14:40 -0500

    added zookeeper functionality

--- server.js
+++ server.js
@@ -10,13 +10,13 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
-
 // 'middleware' to serve static files (font-end files: css and js)
 // this instructs the server to make everything in the 'public' directory into static resources
 // so that a server endpoint does not have to be specified for every single asset (front-end code such as css and js)
 app.use(express.static('public'));
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
